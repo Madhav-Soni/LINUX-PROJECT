511914611723fc757cd207b0a81411b67ef97b3a
Madhav <madhavAO321F@gmail.com>
2026-05-13 17:25:50 +0530

    First commit

--- user-space/internal/httpapi/api.go
+++ user-space/internal/httpapi/api.go
@@ -37,7 +37,7 @@ func NewHandler(deps Dependencies) http.Handler {
 
 func withCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 		w.Header().Set("Vary", "Origin")
