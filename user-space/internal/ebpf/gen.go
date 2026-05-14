dca83a45bd6a4a1298465ede8a0f4b4f974bb166
Madhav <madhavAO321F@gmail.com>
2026-05-14 11:42:22 +0530

    build: update eBPF compilation flags and bump Go version to 1.24.13

--- user-space/internal/ebpf/gen.go
+++ user-space/internal/ebpf/gen.go
@@ -2,4 +2,4 @@ package ebpf
 
 // Compiles execve.bpf.c → execve_bpfel.go + execve_bpfeb.go (little/big endian)
 // Run: go generate ./internal/ebpf/
-//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -cc clang -cflags "-O2 -g -Wall -Werror" execve ./execve.bpf.c -- -I/usr/include/bpf -I/usr/include
+//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -cc clang -cflags "-I/usr/include/aarch64-linux-gnu" execve execve.bpf.c
